8ad547f5c45ce73558d13001218921963db05e95
landerland01 <atvicn@mail.ru>
2023-11-26 14:24:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function rez(){
+
+    alert(1);
+
+    // let x = prompt ();
+    // let y = prompt ();
+    // let op = prompt ();
+   
+    // switch(op){
+    //         case '+':
+    //         console.log("Это +");
+    //         break;
+    //         case '-':
+    //         console.log("Это -");
+    //         break;
+    //         case '*':
+    //         console.log("Это *");
+    //         break;
+    //         case '/':
+    //         console.log("Это /");
+    //         break;
+    //         default:
+    //         console.log("Это /");                
+    //         break;
+    // };
+
+}
